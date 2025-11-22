e42755ef9ea5f365f6d5c204be957a2f98377e61
Nguyen Viet Tung <Tung1.NguyenViet@mbs.com.vn>
2025-11-22 20:13:26 +0700

    update code wordcount

--- aws-spark-automation/code/src/main/java/WordCount.java
+++ aws-spark-automation/code/src/main/java/WordCount.java
@@ -8,7 +8,6 @@ import java.util.Arrays;
 
 public class WordCount {
     public static void main(String[] args) {
-        // Check for correct number of arguments
         if (args.length < 2) {
             System.err.println("Usage: WordCount <input-file> <output-file>");
             System.exit(1);
@@ -17,24 +16,19 @@ public class WordCount {
         String inputFile = args[0];
         String outputFile = args[1];
 
-        // The SparkConf no longer sets a master. This will be provided by spark-submit.
         SparkConf conf = new SparkConf().setAppName("WordCountBenchmark");
         JavaSparkContext sc = new JavaSparkContext(conf);
 
-        // Start timer
         long startTime = System.currentTimeMillis();
 
-        // The core WordCount logic remains the same
         JavaRDD<String> textFile = sc.textFile(inputFile);
         JavaPairRDD<String, Integer> counts = textFile
                 .flatMap(s -> Arrays.asList(s.split("[^a-zA-Z]+")).iterator())
                 .mapToPair(word -> new Tuple2<>(word.toLowerCase(), 1))
                 .reduceByKey(Integer::sum);
 
-        // Save the result. This will create a directory with part-xxxxx files.
         counts.saveAsTextFile(outputFile);
-
-        // Stop timer and print result
+        
         long endTime = System.currentTimeMillis();
         System.out.println("========================================");
         System.out.println("Job completed for input: " + inputFile);
